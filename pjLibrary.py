21f489e4085fe0dbda507f9798e5d5bcdeb598dc
EllaimA <102774306+EllaimA@users.noreply.github.com>
2023-10-28 08:58:41 -0700

    Update pjLibrary.py

--- pjLibrary.py
+++ pjLibrary.py
@@ -1,4 +1,19 @@
 import string
 
-# Create a apphabet list
+# Create a alphabet list
 alphabet = list(string.ascii_lowercase)
+
+# Resolve ties by giving single-letter precedence over letter groups
+  then alphabetically
+# Pass in two strings needs to be compared
+def resolveTie(stringA, stringB):
+    # Check which string is shorter, return two strings as tuple so the shorter
+    # string is the first element in the tuple
+    if len(stringA) < len(stringB):
+        return (stringA, stringB)
+    elif len(stringB) < len(stringA):
+        return (stringB, stringA)
+    else:
+    #resolve ties by multiple letter groups
+        # return 1 for test
+        return 1
