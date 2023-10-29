4a00ea459761b36a64dc9000aebd7736e333cec1
Marc Roube <75597941+gitthekid@users.noreply.github.com>
2023-10-29 12:58:15 -0400

    Add files via upload

---
+++ huffman_node_class.py
@@ -0,0 +1,9 @@
+# Huffman Node class, with two children per node
+class HuffmanNode():
+    def __init__(self, left=None, right=None, root=None):
+        self.left = left
+        self.right = right
+        self.root = root    
+    def children(self):
+        return((self.left, self.right))    # method that returns both children of the node
+
