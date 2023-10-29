f311e944fdab0fc100f25aa2a5ac24eacd1b61c8
Marc Roube <75597941+gitthekid@users.noreply.github.com>
2023-10-29 13:02:12 -0400

    Update huffman_node_class.py

--- huffman_node_class.py
+++ huffman_node_class.py
@@ -1,9 +1,10 @@
 # Huffman Node class, with two children per node
 class HuffmanNode():
-    def __init__(self, left=None, right=None, root=None):
+    def __init__(self, left=None, right=None, root=None, frequency=None):
         self.left = left
         self.right = right
-        self.root = root    
+        self.root = root
+        self.frequency = frequency
     def children(self):
         return self.left, self.right    # method that returns both children of the node
 
